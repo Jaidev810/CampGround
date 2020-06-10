20624510861e44168ac2d70bcf3e1fa201963d65
jaidev <jaidevchaudhary810@gmail.com>
2020-06-10 19:12:32 +0530

    landing page

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,15 +1,42 @@
-const btn1 = document.querySelector('#btn1');
-const btn2 = document.querySelector('#btn2');
-const btn3 = document.querySelector('#btn3');
+const button = document.querySelector('#check');
 
-const tl1 = new TimelineLite({ paused: true, reversed: true});
+const tl = new TimelineLite({ paused: true, reversed: true});
 
 tl.fromTo('#backimg1', 0.5, {
+    width: '100%',
+    heigth: '100%',
+    ease: Power2.easeout
+
+}, {
     width: '0%',
-    heigth: '0%', 
+    heigth: '0%',
+    ease: Power2.easeout
+})
+
+tl.fromTo('#backimg2', 0.5, {
+    width: '0%',
+    heigth: '0%',
     ease: Power2.easeout
 }, {
     width: '100%',
     heigth: '100%',
     ease: Power2.easeout
 })
+
+
+button.addEventListener('click', () =>{
+
+    if(tl.isActive()){
+        e.preventDefault();
+        e.stopImmediatePropagation();
+        return false;
+    }
+    toggleTween(tl);
+
+})
+
+function toggleTween(tween){
+    tween.reversed() ? tween.play() : tween.reverse();
+    //if             then            else  
+}
+
